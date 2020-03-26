afca08b2add366dd665c47ca6565a55275b6c01c
Mircea Gosman <mirceagosman@gmail.com>
2020-03-26 10:12:20 -0400

    Database added to project
    Server now stores new users and their schedule's information to an SQLite database using flask-sqlAlchemy. The database has 3 tables: Users, Days and Courses.
    
    Users: ID
    Days: ID, (userID)
    Courses: ID, Start, End, Text, (weekDay)
    
    
    Database queries and updates are not yet supported by the project as client side code structure needs to be developed first.

---
+++ Server/Database/Models/Models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from Database.Database import Base  # Remove Database. when instantiating database with shell, add for runtime
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    days = relationship('Day', backref='user', lazy=True)
+
+class Day(Base):
+    __tablename__ = 'days'
+    id = Column(Integer, primary_key=True)
+    userID = Column(Integer, ForeignKey('users.id'), nullable=False)
+    courses = relationship('Course', backref='day', lazy=True)
+
+class Course(Base):
+    __tablename__ = 'courses'
+    id = Column(Integer, primary_key=True)
+    start = Column(String, nullable=False)
+    end = Column(String, nullable=False)
+    text = Column(String, nullable=False)
+    weekDay = Column(Integer, ForeignKey('days.id'), nullable=False)
+
+# One to many relationship reference : https://www.youtube.com/watch?v=juPQ04_twtA  Pretty Printed Yt channel
--- Server/app.py
+++ Server/app.py
@@ -1,30 +1,57 @@
 import flask
 import Helpers.OpenCVHelpers as cvH
 
-
 from flask import request
+from Database.Database import db_session
+from Database.StoreAccess import createUser, updateStore, queryStore
+from flask_sqlalchemy import SQLAlchemy
 from ScheduleAnalysis.Schedule import Schedule
 
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/test', methods=['GET', 'POST'])
 def handle_request():
+    # Greet
+    print('Welcome.')
+
+    # Handle request
     if request.method == 'POST':
-        #handle post requests with files
-        # Save file
-        imageFile = request.files['Schedule']
-        imageFileName = 'schedule.'+ request.form['ext']
-        imageFile.save(imageFileName)
-        print('hello')
-        # Hold extracted data
-        topLeft, width, height = cvH.findScheduleBounds(imageFileName)
-        schedule = Schedule(imageFileName, topLeft, width, height)
+        # Check if new user
+        #if request.form['id'] == 'new':
+        if True:
+            createUser(analyseSchedule())
+            return "New user succesfully registered in Database."
+        else:
+            # Check if user is in database
+            if userInDatabase(request.form['id']):
+                # if request.form['update'] == 'true':
+                updateStore(request.form)
+                # elif request.form['read'] == 'true':
+                # queryStore(request.form)
+                return "Recurring user database access validated."
+            else:
+                return "Invalid user database access attempt."
 
-
-        # handle post requests with {'key' : 'value'}
-        return "Post request handled by server succesfully."
     else :
-        # handle get requests
         return "Get request handled by server succesfully."
 
+# Stop database sessions
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+# Extract and organize text from image
+def analyseSchedule():
+    # Save File
+    imageFile = request.files['Schedule']
+    imageFileName = 'schedule.'+ request.form['ext']
+    imageFile.save(imageFileName)
+
+    # Create schedule Object
+    topLeft, width, height = cvH.findScheduleBounds(imageFileName)
+    schedule = Schedule(imageFileName, topLeft, width, height)
+
+    return schedule.days
+
+# Start the server (must be after all routes)
 app.run(host="0.0.0.0", port=5000, debug=True)
