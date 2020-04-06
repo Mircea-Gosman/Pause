2b22118f78df9fa2956dd1480e8efb794149f314
Mircea Gosman <mirceagosman@gmail.com>
2020-04-06 14:21:12 -0400

    Application is now divided into multiple directories:     - APIs (Facebook API)     - Pages (Widgets)     - Painters (Graphical design)     - Server (Server code)     - User
    Application now spawns a Server(User()) in main().
    
    Use of Provider library makes access to the Server instance
    available throughout the widget tree.
    
    Application correctly logs-in the user with FB id in the server's
    Database.
    
    Further development should focus on individual application features.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
-import 'dart:io';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
-import 'package:mime/mime.dart';
-import 'package:http_parser/http_parser.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-import 'dart:convert' as JSON;
-import 'package:location/location.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+import 'user/User.dart';
+import 'server/Server.dart';
+import 'pages/Routes.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(Pause(server: Server(User())));
+
+class Pause extends StatelessWidget {
+  final Server server;
+
+  Pause({Key key, @required this.server}) : super(key: key);
 
 
   // This widget is the root of your application.
@@ -25,412 +24,28 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      home: Login(),
-    );
-  }
-}
+    String initialRoute = '/Login';
+    if (server.user.isLoggedIn) {
+      initialRoute = '/Home';
+    }
 
-// Login page TODO: Handle already logged in case
-class Login extends StatelessWidget {
-  @override
-  Widget build (BuildContext context) {
-    return new Scaffold(
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Display picked image in UI
-              //new Container(
-              //  child: Image.file(imageFile),
-              //),
-              Text(
-                'Login Page',
-              ),
-              RaisedButton(
-                color: Color(0xff3b5998),
-                shape:RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(18.0),
-                ),
-                textColor: Colors.white,
-                onPressed: () {
-                  loginToFB(context);
-                },
-                child: Text('Login with Facebook', style: TextStyle(fontSize: 20)),
-              ),
-            ],
+    return Provider<Server>.value(
+        value: server,
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.green,
           ),
-        ),
-    );
-  }
-
-  Future<void> loginToFB(BuildContext context) async{
-    final facebookLogin = FacebookLogin();
-    final result = await facebookLogin.logIn(['user_friends']);
-
-    switch (result.status) {
-      case FacebookLoginStatus.loggedIn:
-        // Get user data from Facebook
-        getDataFromFB(result.accessToken.token); // TODO: Assign User object to the return of this function
-
-        // Redirect to HomePage
-        goToHomePage(context);                   // TODO: Pass the user object to this function
-
-        break;
-      case FacebookLoginStatus.cancelledByUser:
-        //TODO: Show appropriate error message
-        break;
-      case FacebookLoginStatus.error:
-        //TODO: Show appropriate error message
-        break;
-    }
-  }
-
-  void getDataFromFB(String token) async{
-    final graphResponse = await http.get(
-        'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,picture,friends&access_token=${token}');
-    final profile = JSON.jsonDecode(graphResponse.body);
-    final profilePictureURL = profile['picture']['data']['url']; // Format of picture obj.: {data: {height: 50, is_silhouette: false, url: https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2244735515835110&height=50&width=50&ext=1585085593&hash=AeT70MYhWvAdn6ua, width: 50}}
-    final friendList = profile['friends']['data'];               // Format of friends obj.: {data: [], summary: {total_count: 216}} ;; it only includes friends that use the app & who give permission
-
-    // TODO: create new User object as return to this function
-  }
-
-  void goToHomePage(BuildContext context){
-    Navigator.push(
-      context,
-      new MaterialPageRoute(builder: (context) => new MyHomePage()), // TODO: Pass User Object to homepage
-    );
-  }
-}
-
-// Profile page
-class Profile extends StatelessWidget {
-  @override
-  Widget build (BuildContext context) {
-    return new Scaffold(
-        body: Stack(
-          children: <Widget>[
-            ProfileBar(),
-            ProfileOptions(),
-            //TODO: Real Back Button
-            FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  new MaterialPageRoute(builder: (context) => new MyHomePage(title: "Pause")),
-                );
-              },
-              child: Icon(Icons.arrow_back),
-              tooltip: 'Return to Home Page',
-              //child: Icon(Icons.add),
-              heroTag: "btn3",
-            ),
-          ],
+          //home: Login(),
+          initialRoute: initialRoute,
+          routes: routes,
         ),
     );
   }
 }
 
-// Central app page
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// Provider [ https://pub.dev/packages/provider ] idea:
+// https://medium.com/coding-with-flutter/flutter-global-access-vs-scoped-access-with-provider-8d6b94393bdf
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class CurvePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint();
-    // TODO: Set properties to paint
-    paint.color = Colors.black;
-    paint.style = PaintingStyle.fill;
-
-    var path = Path();
-
-    // TODO: Draw your path
-    path.moveTo(0, size.height * 0.25);
-    path.quadraticBezierTo(
-        size.width / 2, size.height / 2.5, size.width, size.height * 0.25);
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
-// App Bar
-class ProfileBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomPaint(
-        painter: CurvePainter(),
-        child:  Stack(
-          children: <Widget>[
-            // Profile
-            Positioned(
-              top: (MediaQuery.of(context).size.height) * 0.1,
-              left: (MediaQuery.of(context).size.width) * 0.36,
-              width: 100,
-              height: 100,
-              child: FlatButton(
-                color: Colors.white,
-                shape: CircleBorder(),
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    new MaterialPageRoute(builder: (context) => new Profile()),
-                  );
-                },
-              ),
-            ),
-            Positioned(
-              top: (MediaQuery.of( context).size.height) * 0.2,
-              left: (MediaQuery.of(context).size.width) * 0.64,
-              width: 30,
-              height: 30,
-              child:  IconButton(
-                color: Colors.white,
-                //shape: CircleBorder(),
-                //textColor: Colors.white,
-                onPressed: () {
-                  // TODO
-                },
-                icon: Icon(Icons.settings),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// Profile page buttons set
-class ProfileOptions extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-          children: <Widget>[
-            // Add Schedule Button
-            Positioned(
-              top: (MediaQuery.of(context).size.height) * 0.8,
-              left: (MediaQuery.of(context).size.width) * 0.3,
-
-              child:  FlatButton(
-                color: Colors.black,
-                textColor: Colors.white,
-                shape: CircleBorder(),
-                onPressed: () {
-                  reachFirebase();
-                  // TODO: Show additional dialogs, ie. schedule adjustments, etc.
-                },
-                child: new Icon(Icons.add, size: 115),
-              ),
-            ),
-            // Geolocalisation button
-            Positioned(
-              top: (MediaQuery.of(context).size.height) * 0.715,
-              left: (MediaQuery.of(context).size.width) * 0.07,
-              width: 85,
-              height: 85,
-              child:  FlatButton(
-                color: Colors.black,
-                textColor: Colors.white,
-                shape: CircleBorder(),
-                onPressed: () {
-                  setLocationAvailable();
-                  // TODO
-                },
-                child: new Icon(Icons.location_on, size: 42.5),
-              ),
-            ),
-            // Do not disturb button
-            Positioned(
-              top: (MediaQuery.of(context).size.height) * 0.7,
-              left: (MediaQuery.of(context).size.width) * 0.63,
-
-              child:  FlatButton(
-                //color: Colors.white,
-                textColor: Colors.black,
-                shape: CircleBorder(),
-                onPressed: () {
-                  // TODO
-                },
-                child: new Icon(Icons.do_not_disturb_on, size: 100),
-              ),
-            ),
-          ],
-
-        );
-  }
-
-
-  // --------------------------------------- Start of Firebase Code --------------
-
-  // Does this need to be Future<void> ?
-  Future<void> reachFirebase() async {
-    File imageFile =  await pickImage();
-    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
-    final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
-    final VisionText visionText = await textRecognizer.processImage(visionImage);
-    String text = visionText.text;
-    var blocks = new List();
-
-    for (TextBlock block in visionText.blocks) {
-      for (TextLine line in block.lines) {
-        double minX =  0;
-        double maxX =  0;
-        double minY =  0;
-        double maxY =  0;
-
-        if(line.boundingBox.topLeft.dx < line.boundingBox.bottomLeft.dx) {
-          minX = line.boundingBox.topLeft.dx.toDouble();
-        } else {
-          minX = line.boundingBox.bottomLeft.dx.toDouble();
-        }
-
-        if(line.boundingBox.topRight.dx > line.boundingBox.bottomRight.dx) {
-          maxX = line.boundingBox.topRight.dx.toDouble();
-        } else {
-          maxX = line.boundingBox.bottomRight.dx.toDouble();
-        }
-
-        if(line.boundingBox.topRight.dy > line.boundingBox.topLeft.dy) {
-          maxY = line.boundingBox.topRight.dy.toDouble();
-        } else {
-          maxY = line.boundingBox.topLeft.dy.toDouble();
-        }
-
-        if(line.boundingBox.bottomRight.dy < line.boundingBox.bottomLeft.dy) {
-          minY = line.boundingBox.bottomRight.dy.toDouble();
-        } else {
-          minY = line.boundingBox.bottomLeft.dy.toDouble();
-        }
-
-        blocks.add([minX, minY, maxX, maxY]);
-      }
-      print(blocks);
-    }
-    reachServer(text,blocks.toString(), imageFile);
-  }
-
-  Future<File> pickImage() async {
-    return  ImagePicker.pickImage(source: ImageSource.gallery);
-  }
-
-  Future<void> reachServer(String imageText, String linesBoundingBoxes, File imageFile) async {
-    var url = 'http://10.0.2.2:5000/'; // 10.0.2.2 [school] or 192.168.1.17 [home]
-    /* Make standard get request
-    var response = await http.get(url);
-    */
-    /* Make standard post request with text body
-    var postResponse = await http.post(url, body: {'key' : imageText});
-     */
-
-    // Make Multipart request to send file and text
-    final mimeTypeData = lookupMimeType(imageFile.path, headerBytes: [0xFF, 0xD8]).split('/');
-
-    http.MultipartRequest requestFile = http.MultipartRequest('POST', Uri.parse(url));
-    http.MultipartFile multipartFile = await http.MultipartFile.fromPath('Schedule', imageFile.path, contentType : MediaType(mimeTypeData[0], mimeTypeData[1]));
-
-    requestFile.fields['ext'] = mimeTypeData[1];
-    requestFile.files.add(multipartFile);
-    requestFile.fields['imageText'] = imageText;
-    requestFile.fields['linesBoundingBoxes'] = linesBoundingBoxes;
-
-    final streamedResponse = await requestFile.send();
-    final fileResponse = await http.Response.fromStream(streamedResponse);
-
-    print(fileResponse.body);
-  }
-
-  // --------------------------------------- End of Firebase Code --------------
-  // --------------------------------------- Start of GeoLocation Code ---------
-  void setLocationAvailable() async{
-    Location location = new Location();
-
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
-
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.DENIED) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.GRANTED) {
-        return;
-      }
-    }
-
-    // Real-Time location listener
-    location.onLocationChanged().listen((LocationData currentLocation) {
-      // TODO: Use current location
-    });
-  }
-  // --------------------------------------- End of GeoLocation Code -----------
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ProfileBar(),
-    );
-  }
-}
+// Project file structure idea:
+// https://medium.com/flutter-community/flutter-code-organization-revised-b09ad5cef7f6
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pause_v1/main.dart';
+import 'package:pause_v1/server/Server.dart';
+import 'package:pause_v1/user/User.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Pause(server: Server(User())));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
