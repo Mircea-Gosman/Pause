67edb5d18ef6632bef6f67e30b8e2f8bd7738b7d
Mircea Gosman <mirceagosman@gmail.com>
2020-04-22 09:44:20 -0400

    Merging new image analysis system to server.

--- Server/ScheduleAnalysis/Line.py
+++
@@ -1,6 +0,0 @@
-class Line:
-    def __init__(self, topLeft, width, height, text):
-        self.topLeft = topLeft
-        self.width = width
-        self.height = height
-        self.text = text
