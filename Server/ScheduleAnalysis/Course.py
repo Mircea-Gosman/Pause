aa611e2cae39976aa71896e1636fab7feed3bf5c
Mircea Gosman <mirceagosman@gmail.com>
2020-05-15 19:57:26 -0400

    Comments added.

--- Server/ScheduleAnalysis/Course.py
+++ Server/ScheduleAnalysis/Course.py
@@ -1,5 +1,13 @@
+# -----------------------------------------------------------
+# Course object for JSON to be sent to client
+#
+# 2020 Mircea Gosman, Terrebonne, Canada
+# email mirceagosman@gmail.com
+# -----------------------------------------------------------
+
 class Course:
+    # Initializer
     def __init__(self, startTime, endTime, text):
-        self.startTime = startTime
-        self.endTime = endTime
-        self.text = text
+        self.startTime = startTime  # String
+        self.endTime = endTime      # String
+        self.text = text            # String
