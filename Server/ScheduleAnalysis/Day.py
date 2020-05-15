c47c88828cf96c0bb866ffd314d621fa99a1da14
Mircea Gosman <mirceagosman@gmail.com>
2020-05-15 19:58:22 -0400

    Comments added.

--- Server/ScheduleAnalysis/Day.py
+++ Server/ScheduleAnalysis/Day.py
@@ -1,4 +1,12 @@
+# -----------------------------------------------------------
+# Day object for JSON to be sent to client
+#
+# 2020 Mircea Gosman, Terrebonne, Canada
+# email mirceagosman@gmail.com
+# -----------------------------------------------------------
+
 class Day:
+    # Initializer
     def __init__(self, title, courses):
-        self.title = title
-        self.courses = courses
+        self.title = title          # String
+        self.courses = courses      # Array of Course instances 
