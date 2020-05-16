31a4ed77a564bdc2378dfdba06223fcac5d5e17e
Mircea Gosman <mirceagosman@gmail.com>
2020-05-15 21:35:07 -0400

    Half of comments added.

--- Server/ScheduleAnalysis/ImageAnalysis.py
+++ Server/ScheduleAnalysis/ImageAnalysis.py
@@ -1,3 +1,9 @@
+# -----------------------------------------------------------
+# Computer vision process
+#
+# 2020 Mircea Gosman, Terrebonne, Canada
+# email mirceagosman@gmail.com
+# -----------------------------------------------------------
 import operator
 import re
 import cv2 as cv
@@ -7,11 +13,12 @@ from pytesseract import Output
 from Helpers.CustomMath import intersects, findMedian
 
 class ImageAnalysis:
+    # Initializer
     def __init__(self, fileName):
         self.fileName = fileName
 
+    # Meta steps of the computer vision process 
     def initiateAnalysis(self):
-        # All functions must be called in that order, formating & method is verbose
         scheduleCrop, borderSize = self.cropScheduleFromImage(self.fileName)
         corners = self.extractCornersFromSchedule(scheduleCrop)
         gridCorners = self.filterGridCorners(corners, borderSize, scheduleCrop.shape[0])
@@ -25,18 +32,25 @@ class ImageAnalysis:
 
         return days, dayTitles
 
-    # Crop biggest polygon in the image (likely to be the schedule)
+    # Crop the image to its biggest polygon (likely to be the schedule)
     def cropScheduleFromImage(self, fileName):
+        # Read the image file
         img = cv.imread(self.fileName)
+        
+        # Apply filters
         imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
         edges = cv.Canny(imgray,50,150,apertureSize = 3)
+        
+        # Extract image contours
         contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-
+        
+        # Sort contours by area size
         contours = sorted(contours, key=cv.contourArea, reverse=True) # seemingly same contours as after the crop
 
+        # Set biggest contour as a shape containing the schedule
         polygon = contours[0]
 
+        # Identify the shapes's extremities 
         topLeft, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key =operator.itemgetter(1))
         bottomLeft, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key =operator.itemgetter(1))
         topRight, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key =operator.itemgetter(1))
@@ -44,7 +58,7 @@ class ImageAnalysis:
         width = polygon[topRight][0][0] - polygon[topLeft][0][0]
         height = polygon[bottomLeft][0][1] - polygon[topLeft][0][1]
 
-        # Crop the image
+        # Crop the image to the schedule's shape
         cropImg = img[polygon[topLeft][0][1]:polygon[topLeft][0][1]+height, polygon[topLeft][0][0]:polygon[topLeft][0][0]+width]
 
         # Add a border to the cropped image (blue because its unlikely to be present in image)
@@ -52,43 +66,52 @@ class ImageAnalysis:
 
         return cv.copyMakeBorder(cropImg, borderSize, borderSize, borderSize, borderSize, cv.BORDER_CONSTANT,value=[255,0,0]), borderSize
 
-    # Extract the corners from the schedule as coordinates (solution from doc)
+    # Extract the corners from the schedule as coordinates (solution from documentation)
     def extractCornersFromSchedule(self, scheduleCrop):
-        gray = cv.cvtColor(scheduleCrop, cv.COLOR_BGR2GRAY)
-        edges = cv.Canny(gray,25,150,apertureSize = 3)
-
+        # Apply filters
+        grayScale = cv.cvtColor(scheduleCrop, cv.COLOR_BGR2GRAY)
+        edges = cv.Canny(grayScale,25,150,apertureSize = 3)
         gray = np.float32(edges)
+        
+        # Extract corners
         dst = cv.cornerHarris(gray,2,3,0.04)
 
-        #result is dilated for marking the corners, not important
+        # Result is dilated for marking the corners, not important
         dst = cv.dilate(dst,None)
 
         ret, dst = cv.threshold(dst,0.01*dst.max(),255,0)
         dst = np.uint8(dst)
 
-        #find centroids
+        # Find centroids
         ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
 
-        #define the criteria to stop and refine the corners
+        # Define the criteria to stop and refine the corners
         criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
 
-        # Return list of corners i.e. [[x,y], [x,y]] where [x,y] is a corner
+        # Return a list of corners i.e. [[x,y], [x,y], ...] where [x,y] is a corner
         return cv.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
 
+    # Extract line segments from the image as pairs of endpoint coordinates
     def extractLinesFromSchedule(self, scheduleCrop):
-        gray = cv.cvtColor(scheduleCrop, cv.COLOR_BGR2GRAY)                     # avoid repeating this
+        # Apply filters
+        gray = cv.cvtColor(scheduleCrop, cv.COLOR_BGR2GRAY)                     
         edges = cv.Canny(gray,25,150,apertureSize = 3)
+        
+        # Extract line segments
         glines = cv.HoughLinesP(edges,1,np.pi/180,1,1,0)
+        
+        # Filter out vertical lines
         graphLines = []
 
         for i in range(len(glines)):
             for x1,y1,x2,y2 in glines[i]:
-                if y2 - y1 == 0: # filter those without height
+                if y2 - y1 == 0: 
                     graphLines.append([x1,y1,x2,y2])
 
-        # Return list of line segments in the image i.e. [[x1,y1,x2,y2], [x1,y1,x2,y2]]
+        # Return list of line segments in the image i.e. [[x1,y1,x2,y2], [x1,y1,x2,y2], ...]
         return graphLines
-
+    
+    # Format and filter corners into normalized columns
     def filterGridCorners(self, corners, borderSize, imgHeight):
         # Parameters
         baseThresholdY = 3
@@ -96,15 +119,19 @@ class ImageAnalysis:
         minimumSubCorners = 3
         possibleConnectionThreshold = 4
 
+        # Identify columns' bottom corners
         columnStartCorners = self.identifyColumns(baseThresholdY, corners, borderSize, imgHeight)
+        
+        # Build columns from their bottom corners
         unStructuredGridCorners = self.buildGrid(baseThresholdX, corners, columnStartCorners)
 
         # Return a clean version of unStructuredGridCorners (filtered errors & missing corners)
         return self.filterGrid(minimumSubCorners, possibleConnectionThreshold, unStructuredGridCorners)
 
+    # Identify columns' bottom corners
     def identifyColumns(self, baseThresholdY, corners, borderSize, imgHeight):
-        startCorners = []
-        startCornersX = []
+        startCorners = []   # First draw of bottom conrners
+        startCornersX = []  # X coordinate of bottom corners
 
         # Pick corners in the bottom-padding of the image
         for corner in corners:
@@ -112,19 +139,20 @@ class ImageAnalysis:
                 startCorners.append(corner)
                 startCornersX.append(corner[0])
 
-        # Sort the lists
+        # Sort the corners based on their position (left to right) 
         startCorners = [x for _,x in sorted(zip(startCornersX, startCorners))]
         startCornersX.sort()
 
-        # Try to account for extra columns
+        # Try to account for extra columns via median distance btw corners monitoring
         distances = []
         for i in range(len(startCornersX)):
             if i != len(startCornersX) - 1:
                 distances.append(abs(startCornersX[i + 1] - startCornersX[i]))
-
+        
+        # Define an arbitrary ratio for the threshold
         medianThreshold = findMedian(distances)*4/5
-
-        finalCorners = []
+        
+        finalCorners = [] # Last draw of the bottom corners
         for i in range(len(startCorners)):
             if i != len(startCorners) - 1 and i != 0:
                 rightDiff = startCorners[i + 1][0] -  startCorners[i][0]
@@ -138,9 +166,11 @@ class ImageAnalysis:
         # Return remaining list of columns starting corners
         return finalCorners
 
+    # Build columns from their bottom corners
     def buildGrid(self, baseThresholdX, corners, columnStartCorners):
-        unStructuredGridCorners = []
-
+        unStructuredGridCorners = [] # Columns of corners from identified bottom corners
+        
+        # Add corners to columns
         for i in range(len(columnStartCorners)):
             unStructuredGridCorners.append([])
             for corner in corners:
@@ -150,10 +180,11 @@ class ImageAnalysis:
         # Return list of columns containing vertically alined corners within thresholdX
         return unStructuredGridCorners
 
+    # Remove unnecessary columns from columns' list
     def filterGrid(self, minimumSubCorners, possibleConnectionThreshold, unStructuredGridCorners):
-        structuredGridCorners = []
-        longestColumnIndex = 0
-        cornersY = [unStructuredGridCorners[0][0][1]]
+        structuredGridCorners = [] # Filtered columns
+        # longestColumnIndex = 0     
+        cornersY = [unStructuredGridCorners[0][0][1]] # Default comparator for Y coordinate
 
         # Filter corners
         for i in range(len(unStructuredGridCorners)):
@@ -165,6 +196,8 @@ class ImageAnalysis:
                 # Try to account for missing corners in some columns (but present in others)
                 for j in range(len(unStructuredGridCorners[i])):
                     newLineCoordinate  = False
+                    
+                    # Normalize Y coordinates among columns
                     for yCoordinate in cornersY:
                         distanceFromKnownLine = abs(unStructuredGridCorners[i][j][1] - yCoordinate)
                         if (distanceFromKnownLine != 0 and
@@ -184,24 +217,28 @@ class ImageAnalysis:
                 column.append([unStructuredGridCorners[i][0][0], yCoordinate])
             structuredGridCorners.append(column)
 
+        # Return a list of columns containing corners with normalized Y coordinates
         return structuredGridCorners
 
     # Pair corners from 2 adjacent columns
     def establishConnections(self, gridCorners, graphLines):
         # Parameter
         possibleConnectionThreshold = 2
-
-        connectedCorners = []
+        
+        connectedCorners = [] # Wrapper list
         for i in range(len(gridCorners)):
             if i != len(gridCorners) - 1:
-                connectedCorners.append([]) # add pair column
+                connectedCorners.append([]) # Add pair column
+                
                 for cornerA in gridCorners[i]:
                     for cornerB in gridCorners[i + 1]:
                         distanceAB = cornerB[0] - cornerA[0]
-
+                        
+                        # Link corners 2 by 2 based on the proximity of their Y coordinates 
                         if abs(cornerA[1] - cornerB[1]) < possibleConnectionThreshold:
                             connectionPoints = 0
-
+                            
+                            # Proceed to link only if corners are linked by sufficient horizontal line segments
                             for line in graphLines:
                                 lowestCorner = cornerA
                                 highestCorner = cornerB
@@ -209,26 +246,31 @@ class ImageAnalysis:
                                 if(cornerA[1] < cornerB[1]):
                                     lowestCorner = cornerB
                                     highestCorner = cornerA
-
+                                
+                                # Check for intersection of line segments with the corners' average position
                                 intersection = intersects((cornerA[0]+cornerB[0])/2,line)
 
-                                # Pair only if there are enough graph lines btw corners
+                                # Check for X coordinates boundaries
                                 if (line[0] >= cornerA[0] and line[2] <= cornerB[0] and
                                     intersection >= highestCorner[1] - possibleConnectionThreshold and
                                     intersection <= lowestCorner[1] + possibleConnectionThreshold) :
 
                                         linelength = line[2] - line[0]
+                                        
+                                        # Account for single points
                                         if linelength == 0:
-                                            connectionPoints += 1 # single point line
+                                            connectionPoints += 1 
                                         else:
                                             connectionPoints += linelength
-
+                            
+                            # Horizontal line segments must cover at less half the distance btw corners
                             if connectionPoints >= distanceAB/2:
                                 connectedCorners[len(connectedCorners) - 1].append([cornerA, cornerB])
 
+        # Return a list of columns of paired corners
         return connectedCorners
 
-    # Pair connectedCorners with adjacent connectedCorner
+    # Pair each connectedCorner with a connectedCorner from directly adjacent column
     def buildCells(self, connectedCorners):
         gridCells = []
 
